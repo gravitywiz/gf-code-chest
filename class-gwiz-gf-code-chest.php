06188a7fb15e746a1f0a63bbd7ef7ddc4f2ea5cb
veryspry <ehlinger.matt@gmail.com>
2024-02-14 16:43:05 +0100

    Removed scoping prefix from CSS rule declarations that start with `@` such as `@media`, `@font-face`, etc.

--- class-gwiz-gf-code-chest.php
+++ class-gwiz-gf-code-chest.php
@@ -718,9 +718,12 @@ class GWiz_GF_Code_Chest extends GFFeedAddOn {
 		return $form_settings;
 	}
 
-
 	public function prefix_css_selectors( $css, $prefix ) {
-		return preg_replace_callback('/([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/', function( $matches ) use ( $prefix ) {
+		/**
+		 * Find all CSS selectors, but ignore all `@` rules such as @font-face, @media, etc. as they are
+		 * defined globally and not can't be scoped to a certion "parent" selector.
+		 */
+		return preg_replace_callback('/^\s*(?!@)([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/m', function( $matches ) use ( $prefix ) {
 			return $prefix . ' ' . trim( $matches[1] ) . $matches[2];
 		}, $css);
 	}
